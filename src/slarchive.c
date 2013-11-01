07522c8efbadabfdd9ffba405b24419d6a30399a
Chad Trabant <chad@iris.washington.edu>
2013-11-01 17:01:49 +0000

    version 2.2

--- src/slarchive.c
+++ src/slarchive.c
@@ -23,7 +23,7 @@
 #include "dsarchive.h"
 
 #define PACKAGE   "slarchive"
-#define VERSION   "2.1"
+#define VERSION   "2.2"
 
 static void packet_handler (char *msrecord, int packet_type,
 			    int seqnum, int packet_size);
